056758615a78e3aa8c91cda5ef7e6ed977161d04
Areen Raj <areenraj@gmail.com>
2024-08-31 11:31:07 +0530

    breaking stuff

--- build_SU2.sh
+++ build_SU2.sh
@@ -5,5 +5,5 @@ rm -r build
 export PKG_CONFIG_PATH="/home/areen/Programs/OpenBLAS_install/lib:$PKG_CONFIG_PATH"
 export PKG_CONFIG_PATH="/usr/local/cuda/lib64:$PKG_CONFIG_PATH"
 export CUDA_PATH="/usr/local/cuda:$CUDA_PATH"
-./meson.py build -Denable-pywrapper=true -Dwith-mpi=disabled -Denable-openblas=true -Denable-cuda=true --prefix=/home/areen/Programs/SU2_Install_GPU
+./meson.py build --buildtype=debugoptimized -Denable-pywrapper=true -Dwith-mpi=disabled -Denable-openblas=true -Denable-cuda=true -Dtracy_enable=true --prefix=/home/areen/Programs/SU2_Install_GPU
 ./ninja -C build install
